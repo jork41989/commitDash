1747cfec580230303bbd813af6d11a7cd587b997
jork41989 <jork41989@gmail.com>
2021-01-26 13:47:51 -0500

    more css fixes

--- src/components/MainDash/userDashSub.jsx
+++ src/components/MainDash/userDashSub.jsx
@@ -18,7 +18,7 @@ const UserDashSub = (state) =>{
       <div className="userDash">
         <div>{state.data.username}</div>
         <div className="healthBar">
-          <p>{Math.floor(total)}</p>
+          <p className="scoreText">{Math.floor(total)}</p>
           <div className="healthBarColor" style={color}></div>
         </div>
       </div>
